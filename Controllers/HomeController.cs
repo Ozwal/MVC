58befa8cfd1163fe5a35c1c92a47d352607e6e18
Ozwal <ozwaldocastro@gmail.com>
2021-06-03 19:15:03 -0500

    primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    [Route("[controller]/[action]")]
+public class HomeController : Controller
+{
+    [Route("~/")]
+    [Route("/Home")]
+    [Route("~/Home/Index")]
+    public IActionResult Index()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+
+    public IActionResult About()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+}
+}
+public class MyDemo2Controller : Controller
+{
+    [Route("/articles/{page}")]
+    public IActionResult ListArticles(int page)
+    {
+        return ControllerContext.MyDisplayRouteInfo(page);
+    }
+}
+[Route("api/[controller]")]
+[ApiController]
+public class Test2Controller : ControllerBase
+{
+    [HttpGet]   // GET /api/test2
+    public IActionResult ListProducts()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+
+    [HttpGet("{id}")]   // GET /api/test2/xyz
+    public IActionResult GetProduct(string id)
+    {
+       return ControllerContext.MyDisplayRouteInfo(id);
+    }
+
+    [HttpGet("int/{id:int}")] // GET /api/test2/int/3
+    public IActionResult GetIntProduct(int id)
+    {
+        return ControllerContext.MyDisplayRouteInfo(id);
+    }
+
+    [HttpGet("int2/{id}")]  // GET /api/test2/int2/3
+    public IActionResult GetInt2Product(int id)
+    {
+        return ControllerContext.MyDisplayRouteInfo(id);
+    }
+}
+[ApiController]
+public class Products2ApiController : ControllerBase
+{
+    [HttpGet("/products2/{id}", Name = "Products_List")]
+    public IActionResult GetProduct(int id)
+    {
+        return ControllerContext.MyDisplayRouteInfo(id);
+    }
+}
+[ApiController]
+[Route("products")]
+public class ProductsApiController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult ListProducts()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProduct(int id)
+    {
+        return ControllerContext.MyDisplayRouteInfo(id);
+    }
+}
